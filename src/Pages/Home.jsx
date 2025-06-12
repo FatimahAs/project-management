b779e2061f235728c022d1cd5c6ce1eb77d6d838
FatimahAs <46461256+FatimahAs@users.noreply.github.com>
2025-06-12 16:33:58 +0300

    add Link

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,10 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default function Home() {
   return (
     <section className="min-h-screen  flex flex-col justify-center pt-8 lg:pt-32 bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] bg-center bg-cover">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
-        <div className="border border-indigo-600 p-1 w-60 mx-auto rounded-full flex items-center justify-between mb-4">
-          <span className="font-inter text-xs font-medium text-gray-900 ml-3">
-            Explore how to use for brands.
-          </span>
-          <a
-            href="javascript:;"
-            className="w-8 h-8 rounded-full flex justify-center items-center bg-indigo-600"
-          >
-            <svg
-              width="17"
-              height="16"
-              viewBox="0 0 17 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.83398 8.00019L12.9081 8.00019M9.75991 11.778L13.0925 8.44541C13.3023 8.23553 13.4073 8.13059 13.4073 8.00019C13.4073 7.86979 13.3023 7.76485 13.0925 7.55497L9.75991 4.22241"
-                stroke="white"
-                stroke-width="1.6"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </a>
-        </div>
-
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
           <span className="inline-block">
             Manage
@@ -72,6 +47,31 @@ export default function Home() {
           src="https://pagedone.io/asset/uploads/1691054543.png"
           alt="Dashboard image"
         />
+      </div>
+      <div className="border border-indigo-600 mt-5 p-1 w-60 mx-auto rounded-full flex items-center justify-between mb-4">
+        <span className="font-inter text-xs font-medium text-gray-900 ml-3">
+          Explore And Sign in Now
+        </span>
+        <Link
+          to="signin"
+          className="w-8 h-8 rounded-full flex justify-center items-center bg-indigo-600"
+        >
+          <svg
+            width="17"
+            height="16"
+            viewBox="0 0 17 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.83398 8.00019L12.9081 8.00019M9.75991 11.778L13.0925 8.44541C13.3023 8.23553 13.4073 8.13059 13.4073 8.00019C13.4073 7.86979 13.3023 7.76485 13.0925 7.55497L9.75991 4.22241"
+              stroke="white"
+              stroke-width="1.6"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </Link>
       </div>
     </section>
   );
